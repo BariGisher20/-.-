3b1dd8cb2b5b3ae4d82c0dd0752066ea926d83a2
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2021-09-21 21:31:50 +0300

    Task 1.2

--- task.py
+++ task.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -13,15 +14,24 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    # def rate_hw(self, student, course, grade):
-    #     if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-    #         if course in student.grades:
-    #             student.grades[course] += [grade]
-    #         else:
-    #             student.grades[course] = [grade]
-    #     else:
-    #         return 'Ошибка'
- 
 class Lecturer(Mentor):
+    def grades_lec(self):
+        self.grades = {}
+    def rate_lec(self, lecturer, course, grade, student):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
